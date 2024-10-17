acbaef52a64e554bd4cd471559c08e6abaeef340
sebasth <sebasthsegura05@gmail.com>
2024-10-16 21:05:25 -0500

    creacion de la clase Episode que permitira dar una clasificacion mucho mas clara

---
+++ src/com/aluracursos/screenmatch/modelos/Episode.java
@@ -0,0 +1,47 @@
+package com.aluracursos.screenmatch.modelos;
+
+import com.aluracursos.screenmatch.calculos.Clasification;
+
+public class Episode implements Clasification {
+
+    //creamos las variables a utilizar numero, nombre, serie para los episodios
+    private int number;
+    private String name;
+    private Serie serie;
+    private int totalVisualization;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    //ayudara para el filtro de recomendaciones  ya que dependiendo de la
+    //clasificacion que tenga tendra mas visivilidad
+    @Override
+    public int getClasification() {
+        if (totalVisualization > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
+    }
+}
